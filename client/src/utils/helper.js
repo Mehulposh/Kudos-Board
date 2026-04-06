b967df9ab2a024e3b36a50ccb1c30cc53a36e63f
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-04-06 09:20:56 +0530

    feat: created a helper functions file

---
+++ client/src/utils/helper.js
@@ -0,0 +1,18 @@
+// src/utils/helper.js
+export const esc = (s) => {
+  return String(s || '')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+};
+
+export const formatDate = (dateString) => {
+  return new Date(dateString).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric'
+  });
+};
+
+export const generateId = () => Date.now() + Math.random().toString(36).substr(2, 9);

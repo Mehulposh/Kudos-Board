992cc18e63974fb8b7ecd632008b1f8c6af6fd15
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-04-06 09:23:27 +0530

    feat: added confog file for the reading the env variables

---
+++ client/config/index.js
@@ -0,0 +1,3 @@
+// src/config/index.js
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
+export const APP_NAME = import.meta.env.VITE_APP_NAME || 'Kudos Board';
